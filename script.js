f8daaaa136b836deb48f6b9ff16c4f18b2d71139
luiswebneo <luisfernandolopes0@gmail.com>
2024-03-30 19:21:43 -0300

    Finalizado

--- script.js
+++ script.js
@@ -134,19 +134,50 @@ addressInput.addEventListener("input", function (event) {
   }
 })
 
+// Finalizar pedido
 checkoutBtn.addEventListener("click", function () {
+  // const isOpen = checkRestaurantOpen();
+  // if(isOpen){
+  //   alert("RESTAURANTE FECHADO NO MOMENTO")
+  //   return;
+  // }
   if (cart.length === 0) return;
   if (addressInput.value === "") {
     addressWarn.classList.remove("hidden")
     addressInput.classList.add("border-red-500")
     return;
   }
+
+  // enviar o pedido para a api do whats
+  const cartItems = cart.map((item) => {
+    return(
+      ` ${item.name} Quatidade: (${item.quantity}) Preço: R$${item.price} |`
+    )
+  }).join("")
+
+  const message = encodeURIComponent(cartItems)
+  const phone = "47992872163"
+
+  window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+  cart = [];
+  updateCartModal();
 })
 
-function checkRestaurantOpen(){
+function checkRestaurantOpen() {
   const data = new Date();
   const hora = data.getHours();
-  return hora >= 18 && hora < 22;
+  return hora >= 18 && hora < 22; //true restaurante esta aberto
   // true = restaurante está aberto
 }
 
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if (isOpen) {
+  spanItem.classList.remove("bg-red-500");
+  spanItem.classList.add("bg-green-600")
+} else {
+  spanItem.classList.remove("bg-green-600")
+  spanItem.classList.add("bg-red-500")
+}
